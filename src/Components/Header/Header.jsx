ea0ea3e4b6028f165a21c4d1715625ef6fb6b1a5
Siddhant-chandna <87367405+Siddhant-chandna@users.noreply.github.com>
2021-10-07 16:54:10 +0530

    Header Designed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-
+import { Autocomplete } from '@material-ui/lab';
+import { AppBar,Toolbar,Typography,InputBase,Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStlyes from './style';
 const Header = () =>{
+    const classes=useStlyes();
     return(
-        <h1>Header</h1>
+       <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography varient="h5" className={classes.title}>
+                     Travel Companion
+                </Typography>
+                <Box display="flex">
+                <Typography varient="h6" className={classes.title}>
+                     {/* Explore New Places */}
+                </Typography>
+                {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                                <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Explore New Places..." classes={{root:classes.inputRoot,input:classes.inputInput }}/>
+                    </div>
+                {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+       </AppBar>
     );
 }
 
---
+++ src/Components/Header/style.js
@@ -0,0 +1,34 @@
+import { alpha, makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+  title: {
+    display: 'none',
+    fontSize: '23px',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.5),
+    '&:hover': { backgroundColor: alpha(theme.palette.common.black, 0.5) },
+    marginRight: theme.spacing(5),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(5), width: 'auto' },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(7)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '25ch' },
+  },
+  toolbar: {
+    display: 'flex', justifyContent: 'space-between',backgroundColor: '#C64347'
+  },
+}));
