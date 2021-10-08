1cbd63cff2667de89e07bcfb0cff49c1fd4333e4
Siddhant-chandna <87367405+Siddhant-chandna@users.noreply.github.com>
2021-10-08 12:17:55 +0530

    Designed Restaurants Card

--- src/Components/PlaceDetails/PlaceDetails.jsx
+++ src/Components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import {Box,Typography,Card,Button,cardMedia,CardContent,CardActions,Chip} from '@material-ui/core';
-import LocationOnIcon from '@material-ui/icons/LocationOnIcon'
+import {Box,Typography,Card,Button,cardMedia,CardContent,CardActions,Chip, CardMedia} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating'
+import Placestyle from './style.js'
+
 const PlaceDetails = ({place}) =>{
-    console.log(place);
+    const classes=Placestyle();
     return(
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia
+               style={{height:350}}
+               image={place.photo ? place.photo.images.large.url :'https://blog.dineout-cdn.co.in/blog/wp-content/uploads/2019/11/rsz_101-1030x687.jpg'}
+               title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5'>
+                     {place.name}
+                </Typography>
+                {place?. address && (
+                <Typography gutterBottom variant="body1" color="textSecondary" className={classes.subtitle}>   
+                    <LocationOnIcon /> {place.address}
+                </Typography>
+                )}
+                {place?. phone && (
+                <Typography gutterBottom variant="body2" color="textSecondary" className={classes.spacing}>   
+                    <PhoneIcon /> {place.phone}
+                </Typography>
+                )}
+                <Box display='flex' justifyContent='space-between'>
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display='flex' justifyContent='space-between'>
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+
+                {place?.cuisine?.map(({ name }) => (
+                <Chip key={name} size="small" label={name} className={classes.chip} />
+                ))}
+
+               {place?.awards?.map((award)=>(
+                       <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                       <img src={award.images.small} alt={award.display_name} />
+                       <Typography variant="subtitle2" color="texrSecondary">{award.display_name}</Typography>
+                       </Box>
+                ))}
+                 
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url,'_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website,'_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
     );
 }
 
